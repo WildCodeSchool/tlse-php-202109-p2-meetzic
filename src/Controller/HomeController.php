d34e4dc45342e1f962d963c3f68797b56586635b
Barthel31 <sylvain.barthel@gmail.com>
2021-11-02 14:04:00 +0100

    fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
+
     public function publicProfil()
     {
         return $this->twig->render('PublicProfil/publicProfil.html.twig');
