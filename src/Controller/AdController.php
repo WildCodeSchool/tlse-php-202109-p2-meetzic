539d909a86396edc35ed786a5352d28a2cb04d53
Julien Savéan <j.savean@gmail.com>
2021-11-05 12:07:08 +0100

    add cookies index

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -8,8 +8,6 @@ class AdController extends AbstractController
 {
     public function browse()
     {
-        $adModel = new AdModel();
-        var_dump($adModel->getAll());
         return $this->twig->render('Home/ad.html.twig');
     }
 }
