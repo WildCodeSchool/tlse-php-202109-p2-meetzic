152851d770d2c96df6e731b2c998645e7cc248be
Barthel31 <sylvain.barthel@gmail.com>
2021-11-05 15:40:15 +0100

    page profile terminée et responsive

--- src/Model/ProfileManager.php
+++ src/Model/ProfileManager.php
@@ -8,12 +8,13 @@ class ProfileManager extends AbstractManager
     {
         $statement = $this->pdo->prepare(
             'SELECT m.avatar, m.nickname, m.experience, m.status, m.description, 
-            i.name instrument, g.name style, b.name band
+            i.name instrument, g.name style, b.name band, ad.title, ad.description annonce
             FROM musician m 
             LEFT JOIN musician_has_genre mg ON m.id = mg.musician_id
             LEFT JOIN genre g ON g.id = mg.genre_id
             LEFT JOIN instrument i ON i.id = m.instrument_id
             LEFT JOIN band b ON b.id = m.band_id
+            LEFT JOIN ad ON ad.musician_id = m.id
             WHERE m.id = :id;'
         );
         $statement->bindValue(':id', $id, \PDO::PARAM_STR);
@@ -21,21 +22,5 @@ class ProfileManager extends AbstractManager
         $tupple = $statement->fetch(\PDO::FETCH_ASSOC);
 
         return $tupple;
-    }
-
-    public function selectOneAdById(int $id): array
-    {
-        $statement = $this->pdo->prepare(
-            'SELECT ad.title, ad.description
-            FROM ad
-            JOIN musician m 
-            ON m.id = ad.musician_id
-            WHERE m.id = :id;'
-        );
-        $statement->bindValue(':id', $id, \PDO::PARAM_STR);
-        $statement->execute();
-        $card = $statement->fetch(\PDO::FETCH_ASSOC);
-
-        return $card;
     }
 }
